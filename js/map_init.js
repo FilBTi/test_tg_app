dffe50117a28f8f59962c2b4c515d1ca2cd15667
fil <filipp.kosenko@1win.pro>
2024-09-05 17:47:22 +0300

    add_modal_window_1

--- js/map_init.js
+++ js/map_init.js
@@ -1,16 +1,30 @@
 initMap();
 
+const modal = document.querySelector('.modal');
+
+const testObj = [
+    {
+        coordinates: [37.695259, 55.750986],
+    },
+    {
+        coordinates: [37.678870, 55.747737]
+    },
+    {
+        coordinates: [37.692781, 55.748007]
+    }
+];
+
 async function initMap() {
     await ymaps3.ready;
-    
-    const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker, } = ymaps3;
+
+    const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker, YMapControls, YMapGeolocationControl} = ymaps3;
 
     const map = new YMap(
         document.getElementById('map'),
         {
             location: {
                 center: [37.695259, 55.750986],
-                zoom: 17
+                zoom: 15
             }
         }
     );
@@ -19,22 +33,37 @@ async function initMap() {
 
     map.addChild(new YMapDefaultFeaturesLayer());
 
-    const content = document.createElement('div');
 
-    content.className = 'icon'
-    content.innerHTML = '<img src="img/free-icon-beer-bottle-1676691.png" alt="battle" class="icon_map">';
+    testObj.forEach(element => {
+        const markerElement = document.createElement('div');
+        markerElement.className = 'icon';
+        markerElement.onclick = () => {
+            modal.classList.toggle('show');
+        };
+        markerElement.innerHTML = '<img src="img/free-icon-beer-bottle-1676691.png" alt="battle" class="icon_map">';
+        map.addChild(new YMapMarker({coordinates: element.coordinates}, markerElement));
+    });
 
-    const marker = new YMapMarker (
-        {
-            coordinates: [37.695259, 55.750986],
-            draggable: true
-        },
-        content
-    );
+    // fetch('https://geocode-maps.yandex.ru/1.x/?apikey=35969933-e384-42c3-8de8-62b3cc344c6a&geocode=Дубай, бульвар Мухаммед Бин Рашид, дом 1&format=json')
+    //     .then((data => console.log(data)))
 
-    map.addChild(marker);
 
-    
 }
 
 
+// const initPoint = async (dataMarker, map) => {
+//     try{
+//         dataMarker.forEach(element => {
+//             const markerElement = document.createElement('div');
+//             markerElement.className = 'icon';
+//             markerElement.onclick = () => {
+//                 modal.classList.toggle('show');
+//             };
+//             markerElement.innerHTML = '<img src="img/free-icon-beer-bottle-1676691.png" alt="battle" class="icon_map">';
+//             map.addChild(new YMapMarker({coordinates: element.coordinates}, markerElement));
+//         });
+//     }catch(err){
+
+//     }
+    
+// };
--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 window.addEventListener('DOMContentLoaded', () =>{
 
     const icon = document.querySelector('.icon'),
-        modal = document.querySelector('.icon_modal');
+          modal = document.querySelector('.modal');
 
-    const openFunc = () => {
-        modal.classList.toggle('show');
-    }
+    // const openFunc = () => {
+    //     modal.classList.toggle('show');
+    // }
+    const closeFunc = () => {
+        modal.classList.remove('show');
+    };
+
+    document.addEventListener('click', e => {
+        if (e.target === modal){
+            closeFunc();
+        }
+    })
 
     icon.addEventListener('click', openFunc)
     modal.addEventListener('click', () =>{
-        modal.classList.remove('show')
+        modal.classList.remove('show');
     })
 
 })
